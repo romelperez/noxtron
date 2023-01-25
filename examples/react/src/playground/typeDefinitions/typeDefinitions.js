0eddf1aab110ea93426317e3d89599a0722b31f0
Romel Perez <ronelprhone@gmail.com>
2023-01-25 14:21:46 -0500

    chore: fix example react type definitions imports

--- examples/react/src/playground/typeDefinitions/typeDefinitions.js
+++ examples/react/src/playground/typeDefinitions/typeDefinitions.js
@@ -2,53 +2,53 @@ export const typeDefinitions = [
   // react and react-dom
   {
     filename: 'file:///node_modules/csstype/index.d.ts',
-    code: require('!raw-loader?esModule=false!csstype/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/csstype/index.d.ts')
   },
   {
     filename: 'file:///node_modules/@types/prop-types/index.d.ts',
-    code: require('!raw-loader?esModule=false!@types/prop-types/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@types/prop-types/index.d.ts')
   },
   {
     filename: 'file:///node_modules/@types/react/index.d.ts',
-    code: require('!raw-loader?esModule=false!@types/react/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@types/react/index.d.ts')
   },
   {
     filename: 'file:///node_modules/@types/react/global.d.ts',
-    code: require('!raw-loader?esModule=false!@types/react/global.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@types/react/global.d.ts')
   },
   {
     filename: 'file:///node_modules/@types/scheduler/tracing.d.ts',
-    code: require('!raw-loader?esModule=false!@types/scheduler/tracing.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@types/scheduler/tracing.d.ts')
   },
   {
     filename: 'file:///node_modules/@types/react-dom/index.d.ts',
-    code: require('!raw-loader?esModule=false!@types/react-dom/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@types/react-dom/index.d.ts')
   },
 
   // @emotion/react
   {
     filename: 'file:///node_modules/@emotion/react/index.d.ts',
-    code: require('!raw-loader?esModule=false!@emotion/react/types/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@emotion/react/types/index.d.ts')
   },
   {
     filename: 'file:///node_modules/@emotion/react/jsx-namespace.d.ts',
-    code: require('!raw-loader?esModule=false!@emotion/react/types/jsx-namespace.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@emotion/react/types/jsx-namespace.d.ts')
   },
   {
     filename: 'file:///node_modules/@emotion/react/theming.d.ts',
-    code: require('!raw-loader?esModule=false!@emotion/react/types/theming.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@emotion/react/types/theming.d.ts')
   },
   {
     filename: 'file:///node_modules/@emotion/react/helper.d.ts',
-    code: require('!raw-loader?esModule=false!@emotion/react/types/helper.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@emotion/react/types/helper.d.ts')
   },
   {
     filename: 'file:///node_modules/@emotion/serialize/index.d.ts',
-    code: require('!raw-loader?esModule=false!@emotion/serialize/types/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@emotion/serialize/types/index.d.ts')
   },
   {
     filename: 'file:///node_modules/@emotion/utils/index.d.ts',
-    code: require('!raw-loader?esModule=false!@emotion/utils/types/index.d.ts')
+    code: require('!raw-loader?esModule=false!../../../../../node_modules/@emotion/utils/types/index.d.ts')
   },
 
   // empanada (Mock)
